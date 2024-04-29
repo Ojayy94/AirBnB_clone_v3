a64e836c8ef290bfb9fac7467b1f0751a4fd4f26
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-29 14:15:27 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,6 +24,11 @@ def not_found(e):
 
 
 if __name__ == "__main__":
-    """calls the function"""
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=getenv("HBNB_API_PORT", "5000"), threaded=True)
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
