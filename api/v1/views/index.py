66710ca22af26af3f94d1f6198b2b70bc0a4ead1
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-29 12:45:39 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,15 +4,21 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Routing"""
-    return jsonify({'status': 'OK'})
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stat', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def count():
     """Create an endpoint that retrieves the number of each objects by type"""
     classes = [Amenity, City, Place, Review, State, User]
