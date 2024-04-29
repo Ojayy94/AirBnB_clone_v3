448375571ec9c84c00bdc91678e533c4c2e98054
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-29 14:46:59 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,10 +9,10 @@ from models import storage
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Routing"""
-    return jsonify({'status': 'OK'})
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
 def count():
     """Create an endpoint that retrieves the number of each objects by type"""
     classes = [Amenity, City, Place, Review, State, User]
