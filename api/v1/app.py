01d18911fb14d15e50adfa40debb25f08947a675
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-29 12:58:59 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,12 @@ from api.v1.views import app_views
 from flask import Flask, abort, jsonify
 from flask import make_response, render_template
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
