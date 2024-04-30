1244049415fc0424311b1a87c310b7f6fae2933d
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-30 00:09:59 +0000

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/states', methods=['GET'], strict_slashes=False)
 def states():
     """"Retrieves the list of all State objects"""
     states = storage.all(State).values()
@@ -16,7 +16,8 @@ def states():
     return jsonify(states_list)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/states/<state_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object"""
     states = storage.get(State, state_id)
@@ -25,7 +26,7 @@ def get_state(state_id):
     return jsonify(states.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route('/api/v1/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_state(state_id):
     """Deletes a State"""
@@ -39,7 +40,7 @@ def del_state(state_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/api/v1/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a state"""
     if not request.get_json():
@@ -54,7 +55,8 @@ def create_state():
     return make_response(jsonify(create.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/api/v1/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_state(state_id):
     """update a state"""
 
